84100ed08d937061be1660e26035de650d75c57b
shentongen <tongenshen@outlook.com>
2026-08-03 22:58:54 +0800

    update

--- Program1/main.c
+++ Program1/main.c
@@ -1,6 +1,24 @@
 #include <stdio.h>
+
 int main() {
-    printf("Hello, World!\n");
-    printf("This is a C program using the C17 standard.\n");
+    while (1)
+    {
+        int n;
+
+        printf("请输入指数 n：");
+        scanf("%d", &n);
+
+        if (n < 0) {
+            printf("暂不支持负数指数\n");
+            return 1;
+        }
+
+        long long result = 1;
+        for (int i = 0; i < n; i++) {
+            result *= 16;
+        }
+
+        printf("16 的 %d 次方 = %lld\n", n, result);
+    }
     return 0;
 }
